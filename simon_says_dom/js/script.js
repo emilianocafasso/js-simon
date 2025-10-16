87b916d881f2d10c873fff2ef274b17f28e4ee9f
emilianocafasso <emiliano2.cafasso2@gmail.com>
2025-10-16 16:02:43 +0200

    add layout

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -1,5 +1,4 @@
-/*
-Descrizione:
+/* Descrizione:
 Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
 Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
 
